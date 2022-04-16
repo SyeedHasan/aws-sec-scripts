f8e09ecf5859286bd96ab9cf80246da90987894b
Syed H <naqvi_hasan@live.com>
2022-04-16 21:50:09 +0500

    Structural changes to script

--- getWebAcls.py
+++ getWebAcls.py
@@ -1,60 +1,90 @@
 #!/bin/python3
 
 from os import name
+from urllib import request
 import boto3
 import argparse
 import os
 
-print("AWS Helper Script: IP Blocker")
+
+def returnRuleId():
+    pass
+
+# Returns a single web ACL as per the selected ID
+def returnWebAcl(waf, selectedAcl, allAcls):
+
+    id = ""    
+
+    # Searching ACLs by count
+    # Risky; though lists are ordered, the count might lead to unexpected results
+    for count, acl in enumerate(allAcls):
+        if count == int(selectedAcl):
+            id = acl['WebACLId']
+
+    # Searching ACLs by name
+    # Could lead to clashes in ACL names
+    for acl in allAcls:
+        if acl['Name'] == selectedAcl:
+            print(f"WebACLId against the selected ACL number: {acl['WebACLId']}")
+            id = acl['WebACLId']
+            
+    webAcl = waf.get_web_acl(WebACLId=id)
+    print("")
+    print("WebACL:")
+    print("---")
+    print(webAcl['WebACL'])
+
+
+# Returns all web ACL IDs
+def returnAllWebAcls(waf):
+    webAcls = waf.list_web_acls()
+
+    # Generic Variables
+    allAcls = []
+
+    print(f"")
+    print(f"Available ACLs:")
+    print(f"---")
+    for count, acl in enumerate(webAcls['WebACLs']):
+        
+        allAcls.append(acl)
+        print(f'''
+            Count: {count}
+            Name: {acl['Name']} 
+            ID: {acl['WebACLId']})''')
+    
+    return allAcls
+
+# Configures the WAF client
+def configureBoto(wafType):
+    # Limitations: The client only operates on the classic version of WAF (not v2)
+    waf = boto3.client(wafType)
+    return waf
 
 # Arguments for the script
-argParser = argparse.ArgumentParser()
-argParser.add_argument('--type', required=True, default='waf', choices=['waf', 'waf-regional'], help='Select the type of WAF (Classic, V2, Regional')
-argParser.add_argument('--region', required=False, default='', choices=['global', 'us-east-1'], help='Select the region to search for ACLs')
-args = argParser.parse_args()
+def parseArguments():    
+    argParser = argparse.ArgumentParser()
+    argParser.add_argument('--type', required=True, default='waf', choices=['waf', 'waf-regional'], help='Select the type of WAF (Classic, V2, Regional')
+    argParser.add_argument('--region', required=False, default='', choices=['global', 'us-east-1'], help='Select the region to search for ACLs')
 
+    wafInputs = argParser.add_argument_group('WAF-specific Data Objects [Optional]')
+    wafInputs.add_argument('--web-acl-id', required=False, help='Input the WebACLId [Optional]')
+    wafInputs.add_argument('--rule-id', required=False, help='Input the RuleId [Optional]')
 
-# Limitations: The client only operates on the classic version of WAF (not v2)
-waf = boto3.client(args.type)
-webAcls = waf.list_web_acls()
+    args = argParser.parse_args()
+    return args
 
-# Generic Variables
-allAcls = []
-id = ""
-
-print(f"")
-print(f"Available ACLs in {args.type}")
-print(f"---")
-for count, acl in enumerate(webAcls['WebACLs']):
+def main():    
+    print("")
+    print("AWS Helper Script: IP Blocker")
     
-    allAcls.append(acl)
-    
-    print(f'''
-        Count: {count}
-        Name: {acl['Name']} 
-        ID: {acl['WebACLId']})''')
+    args = parseArguments()
+    wafClient = configureBoto(args.type)
+    webAcls = returnAllWebAcls(wafClient)    
 
-print(f"")
-selectedAcl = input("Select an ACL by entering its number (Count): ")
+    selectedAcl = input("Select an ACL by entering its number (Count): ")
+    webAcl = returnWebAcl(wafClient, selectedAcl, webAcls)
 
-# Searching ACLs by count
-# Risky; though lists are ordered, the count might lead to unexpected results
-for count, acl in enumerate(allAcls):
-    print(selectedAcl)
-    if count == int(selectedAcl):
-        print(acl)
-        id = acl['WebACLId']
 
-# Searching ACLs by name
-# Could lead to clashes in ACL names
-for acl in allAcls:
-    if acl['Name'] == selectedAcl:
-        print(f"WebACLId against the selected ACL: {acl['WebACLId']}")
-        id = acl['WebACLId']
-        
-webAcl = waf.get_web_acl(WebACLId=id)
-print("")
-print("WebACL:")
-print("---")
-print(webAcl['WebACL'])
-
+if __name__ == "__main__":
+    main()
